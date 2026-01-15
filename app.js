920c1969584f77ecc87f5aa15ce779a49bf24d01
databastionhq <databastionhq@gmail.com>
2026-01-15 10:17:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log("NEW APP.JS LOADED");
 const API_URL = "https://script.google.com/macros/s/AKfycbzc9MBVbj0ZmgTcn1f_Fwl1oR78rXSnp-wCD0u9QN6Y_XjESlXX-47o7VNgB4Bx-RzRLQ/exec";
 
 function showTab(tab) {
